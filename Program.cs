8b1cb3d258b62537d73708c624ab98e01d8f4aae
Tim Rogers <tiroger@microsoft.com>
2012-01-13 15:19:11 -0800

    Functioning program.  Added GAC support.  Added license info.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
-﻿using System;
+﻿/**
+ * Copyright 2012 Tim Rogers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Reflection;
 
 namespace ListMembers
@@ -48,7 +63,7 @@ namespace ListMembers
             }
 
             List<string> returning = new List<string>();
-            IEnumerable<string> files = Directory.EnumerateFiles(dirPath);
+            IEnumerable<string> files = Directory.GetFiles(dirPath);
             foreach (string file in files)
             {
                 if (file.StartsWith(prePath) && file.EndsWith(postPath))
@@ -132,7 +147,23 @@ namespace ListMembers
                         Libs.Add(LoadAssembly(path));
                     }
                     catch (Exception E)
-                    {}
+                    {
+                        try
+                        {
+                            string newPath = path;
+                            // Since we're checking the GAC, drop the file extension (if any).
+                            if (path[path.Length-4] == '.')
+                                newPath = path.Substring(0, path.Length - 4);
+
+                            newPath = AssemblyCache.QueryAssemblyInfo(newPath);
+                            Libs.Add(LoadAssembly(newPath));
+                        }
+                        catch(Exception e)
+                        {
+                            Console.Error.WriteLine(E.Message);
+                            Console.Error.WriteLine(e.Message);
+                        }
+                    }
             }
 
             foreach (Assembly A in Libs)
